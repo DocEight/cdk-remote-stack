3ca4bf8b3eca6e3d8eb2968d4fa21f2964a93c7d
DocEight <jaholmes1998@gmail.com>
2025-11-16 11:24:47 +0900

    Remove gitpod (for now)

--- .projenrc.js
+++ .projenrc.js
@@ -41,39 +41,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
   },
 });
 
-const gitpodPrebuild = project.addTask("gitpod:prebuild", {
-  description: "Prebuild setup for Gitpod",
-});
-// install and compile only, do not test or package.
-gitpodPrebuild.exec("yarn install --frozen-lockfile --check-files");
-gitpodPrebuild.exec("npx projen compile");
-
-let gitpod = new Gitpod(project, {
-  dockerImage: DevEnvironmentDockerImage.fromImage(
-    "public.ecr.aws/pahudnet/gitpod-workspace:latest",
-  ),
-  prebuilds: {
-    addCheck: true,
-    addBadge: true,
-    addLabel: true,
-    branches: true,
-    pullRequests: true,
-    pullRequestsFromForks: true,
-  },
-});
-
-gitpod.addCustomTask({
-  init: "yarn gitpod:prebuild",
-  // always upgrade after init
-  command: "npx projen upgrade",
-});
-
-gitpod.addVscodeExtensions(
-  "dbaeumer.vscode-eslint",
-  "ms-azuretools.vscode-docker",
-  "AmazonWebServices.aws-toolkit-vscode",
-);
-
 const common_exclude = ["cdk.out", "cdk.context.json", "yarn-error.log"];
 project.npmignore.exclude(...common_exclude);
 project.gitignore.exclude(...common_exclude);
